3f94e07958e8e2f40a61238241191cf80c978545
SWSRN <outfliers@gmail.com>
2014-11-15 13:51:31 -0500

    Add in a couple of edx R files to have com thing to look at

---
+++ WK1_practice1.R
@@ -0,0 +1,13 @@
+AreaCircle  <- function(radius) {
+radius*radius*pi
+}
+
+AreaSquare <- function(l) {
+l*l
+}
+
+AreaTriangle <- function(l, h){ 0.5 * l * h}
+
+AreaCircle(1.0)
+AreaSquare(1.0)
+AreaTriangle(1.0, 1.0)
